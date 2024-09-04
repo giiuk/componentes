e41497a50cefed77cb08518a6c49f5086226a8f5
Laboratório FIAP <labsfiap@fiap.com.br>
2024-09-04 12:23:51 -0300

    incompleto

---
+++ componentesCSS/src/App.jsx
@@ -0,0 +1,16 @@
+function App() {
+
+  return (
+    <>
+    <div className=" box h-50 w-full p-16 bg-pink-600">
+    </div>
+
+    <div className=" w-40 h-screen flex-col p-16 bg-green-300">
+    <div class="box-border h-40 w-40 p-4 border-4"></div>
+    </div>
+
+    </>
+  )
+}
+
+export default App
